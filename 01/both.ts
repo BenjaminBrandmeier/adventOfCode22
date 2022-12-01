81159e4f9625229f972a6652d669cec803f49bb5
Benjamin Brandmeier <bdvlop@gmail.com>
2022-12-01 06:56:50 +0100

    day 1

--- 01/both.ts
+++ 01/both.ts
@@ -1,1 +1,9 @@
-// tbd
+export const loadData = (): number[][] => Deno.readTextFileSync('01/input.txt').split(/\n\n/).map(x => x.split(/\n/).map(Number))
+
+const highest = (a: number, b: number) => b - a
+const sum = (n: number[]) => n.reduce((a, b) => a + b)
+const sumOfTop3 = (n: number[]) => sum(n.slice(0, 3))
+const sortHighest = (n: number[][]) => n.map(sum).sort(highest)
+
+export const first = (provision: number[][]) => sortHighest(provision)[0]
+export const second = (provision: number[][]) => sumOfTop3(sortHighest(provision))
